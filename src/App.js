bb4f29cfc4d4136af52da3e0075cd894f7c78992
GreyRuler <daniil_vasilenko757@mail.ru>
2023-06-10 16:51:48 +0300

    impl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css'
 import './assets/css/google-icons.css'
+import HomePage from './components/HomePage';
+import { Route, Routes } from 'react-router-dom';
+import PostForm from './components/PostForm';
+import PostView from './components/PostView';
+import PostEditView from './components/PostEditView';
 
 function App() {
 	return (
-		<div className='app'></div>
+		<div className='app'>
+			<Routes>
+				<Route path='/' element={<HomePage/>}/>
+				<Route path='/posts/new' element={<PostForm/>}/>
+				<Route path='/posts/:postId' element={<PostView/>}/>
+				<Route path='/posts/edit/:postId' element={<PostEditView/>}/>
+			</Routes>
+		</div>
 	);
 }
 
